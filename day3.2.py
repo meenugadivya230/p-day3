ec0760b9326a2b9ebc7053019e90403b25f4b63a
meenugadivya230 <meenugadivya230@gmail.com>
2025-08-06 16:12:45 +0530

    Add files via upload

---
+++ day3.2.py
@@ -0,0 +1,5 @@
+list_1 = [3,6,4,7,6,7,3]
+first_sum = sum(list_1)
+second_sum = sum(set(list_1))*2
+res = second_sum+first_sum
+print(res)
---
+++ day3.3.py
@@ -0,0 +1,9 @@
+
+n =5
+fact = 1
+for i in range(n,1,-1):
+    fact = fact*i
+print(fact)
+
+
+
